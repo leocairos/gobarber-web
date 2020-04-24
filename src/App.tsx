818bc32b8ada00cac68ed3c59b24d6efbb101406
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-04-24 20:45:46 -0300

    Auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import SigIn from './pages/Signin';
-import SigUp from './pages/Signup';
+import Routes from './routes';
 import GlobalStyle from './styles/global';
+import AppProvider from './hooks';
 
 const App: React.FC = () => (
-  <>
-    <SigUp />
+  <BrowserRouter>
+    <AppProvider>
+      <Routes />
+    </AppProvider>
+
     <GlobalStyle />
-  </>
+  </BrowserRouter>
 );
 
 export default App;
